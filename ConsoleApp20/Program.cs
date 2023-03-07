693e93e33c842b3c7101ec560300073193263277
Eryksabin <eryk.sabiniewicz@gmail.com>
2023-03-07 19:38:49 +0100

    test

--- ConsoleApp20/Program.cs
+++ ConsoleApp20/Program.cs
@@ -2,12 +2,6 @@
 
 namespace ConsoleApp20 {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
 
-        void siema() {
-        
-        }
     }
 }
